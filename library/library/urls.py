fa49628cebdb34d91bfb99f63ec1f27a1478319a
hnedenko <73238004+hnedenko@users.noreply.github.com>
2024-04-09 19:45:55 +0300

    create author api - PATCH

--- library/library/urls.py
+++ library/library/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from .views import AuthorsAPI
+from .views import AuthorsAPI, BooksAPI
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('authors/', AuthorsAPI.as_view()),
+    path('books/', BooksAPI.as_view()),
 ]
--- library/library/views.py
+++ library/library/views.py
@@ -13,6 +13,41 @@ class AuthorsAPI(APIView):
         permission_classes = [AllowAny]
 
     def post(self, request):
-        Author(name=request.data['name'],
-               gender=request.data['gender']).save()
+        author = Author(name=request.data['name'],
+                        gender=request.data['gender'])
+        author.save()
+
+        if request.data['books']:
+            for book_id in json.loads(request.data['books']):
+                book = Book.objects.get(id=book_id)
+                author.books.add(book)
+
         return Response({'POST': 'Authors: Hello World!'}, 200)
+
+    def patch(self, request):
+        author = Author.objects.get(id=request.query_params['id'])
+        if 'name' in request.data:
+            author.name = request.data['name']
+
+        if 'gender' in request.data:
+            author.gender = request.data['gender']
+
+        if 'books' in request.data:
+            books = list()
+            for book_id in json.loads(request.data['books']):
+                books.append(Book.objects.get(id=book_id))
+            author.books.set(books)
+
+        author.save()
+
+        return Response({'PATCH': 'Authors: Hello World!'}, 200)
+
+
+class BooksAPI(APIView):
+
+    def initial(self, request, *args, **kwargs):
+        permission_classes = [AllowAny]
+
+    def post(self, request):
+        Book(title=request.data['title']).save()
+        return Response({'POST': 'Books: Hello World!'}, 200)
