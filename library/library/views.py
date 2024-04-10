130c35f3c82c6d5c0b535b03019166298dc8d991
hnedenko <73238004+hnedenko@users.noreply.github.com>
2024-04-10 09:20:56 +0300

    create author api - GET, POST, PUT, DELETE

--- library/library/views.py
+++ library/library/views.py
@@ -12,17 +12,46 @@ class AuthorsAPI(APIView):
     def initial(self, request, *args, **kwargs):
         permission_classes = [AllowAny]
 
+    def get(self, request):
+
+        if 'id' not in request.query_params:
+            resp = Author.objects.values()
+        else:
+            id = request.query_params['id']
+            resp = Author.objects.filter(id=id).values()
+
+        return Response(resp, 200)
+
     def post(self, request):
         author = Author(name=request.data['name'],
                         gender=request.data['gender'])
-        author.save()
 
-        if request.data['books']:
+        if 'books' in request.data:
             for book_id in json.loads(request.data['books']):
                 book = Book.objects.get(id=book_id)
                 author.books.add(book)
+        author.save()
 
         return Response({'POST': 'Authors: Hello World!'}, 200)
+
+    def put(self, request):
+        old_author = Author.objects.get(id=request.query_params['id'])
+
+        # load new data about author
+        new_author = Author(id=old_author.id,
+                            name=request.data['name'],
+                            gender=request.data['gender'])
+
+        if 'books' in request.data:
+            for book_id in json.loads(request.data['books']):
+                book = Book.objects.get(id=book_id)
+                new_author.books.add(book)
+
+        # replace all old author to new author
+        old_author = new_author
+        old_author.save()
+
+        return Response({'PUT': 'Authors: Hello World!'}, 200)
 
     def patch(self, request):
         author = Author.objects.get(id=request.query_params['id'])
@@ -41,6 +70,13 @@ class AuthorsAPI(APIView):
         author.save()
 
         return Response({'PATCH': 'Authors: Hello World!'}, 200)
+
+    def delete(self, request):
+        id = json.loads(request.query_params['id'])
+
+        Author.objects.filter(id=id).delete()
+
+        return Response({'DELETE': 'Authors: Hello World!'}, 200)
 
 
 class BooksAPI(APIView):
